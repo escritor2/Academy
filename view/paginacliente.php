2dacf2ad70abaa75ee432ef739c4f7551f30e893
Miguel <miguelmassane05@gmail.com>
2025-12-10 17:03:36 -0300

    teste34

--- view/paginacliente.php
+++ view/paginacliente.php
@@ -283,6 +283,7 @@ $subTab = $_GET['sub'] ?? 'produtos';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="icons/halter.png">
     <title>Área do Aluno - TechFit</title>
     <script src="https://unpkg.com/lucide@latest/dist/umd/lucide.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
